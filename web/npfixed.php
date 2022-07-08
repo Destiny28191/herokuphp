b22b6aa46664b9b2ff71ad0836097e313100d0dc
Destiny28191 <20022098@myrp.edu.sg>
2022-07-08 15:07:49 +0800

    initial commit

--- web/npfixed.php
+++ web/npfixed.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <!-- fix: [user-scalable="no"] is used in the <meta name="viewport"> 
+         element or the [maximum-scale] attribute is less than 5 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Put your description here.">">
     <title>Document</title>
 </head>
 <body>
-    <button></button>
-    <a href="https://example.com"></a>
+    <!-- fix: Buttons do not have an accessible name -->
+    <button> Click Here! </button>
+    <!-- fix: Links do not have a discernible name -->
+    <a href="https://example.com"> Example Link </a>
+    <!-- fix: [id] attributes on active, focusable elements are not unique -->
     <input type = "text" id = "ipHomeSearch" placeholder = "Type your keyword here">
-    <input type = "text" id = "ipHomeSearch" placeholder = "Type your keyword here">
-    <ul><p>List item 1</p></ul>
-    <li> Another List item 1 <li>
-    <p>To see all of our basketball videos, <a href="videos.html">click here</a>.</p>
-    <href="https://example.com">
+    <input type = "text" id = "ipHomeSearch2" placeholder = "Type your keyword here">
+    <!-- fix: Lists do not contain only <li> elements and script supporting elements (<script> and <template>). -->
+    <ul>
+        <li>
+            <p>List item 1</p>
+        </li>
+    </ul>
+    <!-- fix: List items (<li>) are not contained within <ul> or <ol> parent elements. -->
+    <ul>
+        <li> Another List item 1 <li>
+    </ul>
+    <!-- fix: Links do not have descriptive text -->
+    <p>Check out all of our <a href="videos.html">basketball videos</a>.</p>
+    <!-- fix: Document does not have a valid rel=canonical -->
+    <link rel="canonical" href="https://example.com"/>
 </body>
 </html>
