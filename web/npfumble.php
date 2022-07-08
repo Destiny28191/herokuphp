5975aa70d56a4edee904532f38d4f79d2335d1eb
Destiny28191 <20022098@myrp.edu.sg>
2022-07-08 14:11:44 +0800

    initial commit

---
+++ web/npfumble.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="Put your description here.">">
+    <title>Document</title>
+</head>
+<body>
+    <button></button>
+    <a href="https://example.com"></a>
+    <input type = "text" id = "ipHomeSearch" placeholder = "Type your keyword here">
+    <input type = "text" id = "ipHomeSearch" placeholder = "Type your keyword here">
+    <ul><p>List item 1</p></ul>
+    <p>To see all of our basketball videos, <a href="videos.html">click here</a>.</p>
+    <href="https://example.com">
+</body>
+</html>
