1003f059e97730e241cc93b03dc04bb225015f66
Destiny28191 <20022098@myrp.edu.sg>
2022-07-08 12:22:15 +0800

    initial commit

--- web/test.php
+++ web/test.php
@@ -1,10 +1,13 @@
+<!-- fix: Page lacks the HTML doctype, thus triggering quirks-mode -->
 <!DOCTYPE html>
+<!-- fix: <html> element does not have a [lang] attribute -->
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- fix: Does not have a <meta name="viewport"> tag with width or initial-scale -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- fix: Document does not have a meta description -->
     <meta name="description" content="test for heroku">
+    <!-- fix: Document does not have title element -->
     <title>Document</title>
 </head>
 <body>
